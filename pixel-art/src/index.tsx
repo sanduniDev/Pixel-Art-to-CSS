61e31bbefc81a968b284bc8a1aee6c066cf0064c
Sanduni Sathsarani <99157473+sanduniDev@users.noreply.github.com>
2024-06-12 16:55:04 +0530

    feat: porting index file to typescript

---
+++ pixel-art/src/index.tsx
@@ -0,0 +1,30 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+import './css/imports.css';
+import configureStore from './store/configureStore';
+import Root from './components/Root';
+import { Store } from 'redux';
+
+// Define interface for the environment variables if needed
+interface ProcessEnv {
+  NODE_ENV?: string;
+  GOOGLE_ANALYTICS_ID?: string;
+}
+
+declare const process: {
+  env: ProcessEnv;
+};
+
+const devMode = process.env.NODE_ENV === 'development';
+const store = configureStore(devMode) as Store; 
+
+if (process.env.GOOGLE_ANALYTICS_ID) {
+  ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID);
+}
+
+
+const mountNode = document.getElementById('app') as HTMLElement;
+
+ReactDOM.render(<Root store={store} />, mountNode);
